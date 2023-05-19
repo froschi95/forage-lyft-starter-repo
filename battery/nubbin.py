f9295f6c5b3991ed6491ae51f62990f7e0a8d1a8
froschi95 <franksuccess95@gmail.com>
2023-05-19 22:25:28 +0100

    Update Tests

--- battery/nubbin.py
+++ battery/nubbin.py
@@ -1,12 +1,16 @@
-from battery_interface import Battery
+from .battery_interface import Battery
 from datetime import date
 
 class NubbinBattery(Battery):
     
-    def __init__(self, last_service_date: date, current_date) -> None:
+    def __init__(self, last_service_date, current_date) -> None:
         self.last_service_date = last_service_date
         self.current_date = current_date
 
     
     def needs_service(self):
-        return self.last_service_date.year + 4 < self.current_date
+        date_by_which_battery_should_be_serviced = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if date_by_which_battery_should_be_serviced.year <= self.current_date.year:
+            return True
+        else:
+            return False
--- battery/spindler.py
+++ battery/spindler.py
@@ -1,12 +1,16 @@
-from battery_interface import Battery
+from .battery_interface import Battery
 from datetime import date
 
 class SpindlerBattery(Battery):
     
-    def __init__(self, last_service_date: date, current_date) -> None:
+    def __init__(self, last_service_date, current_date) -> None:
         self.last_service_date = last_service_date
         self.current_date = current_date
 
     
     def needs_service(self):
-        return self.last_service_date.year + 3 < self.current_date
+        date_by_which_battery_should_be_serviced = self.last_service_date.replace(year=self.last_service_date.year + 3)
+        if date_by_which_battery_should_be_serviced <= self.current_date:
+            return True
+        else:
+            return False
